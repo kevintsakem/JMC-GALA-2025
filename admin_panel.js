29062182d764d9dcbe6a3ce4f34f68839b2155c9
Jack kevin Tsakem Kengne <111010770+kevintsakem@users.noreply.github.com>
2025-12-05 10:15:33 +0100

    Update admin_panel.js

--- admin_panel.js
+++ admin_panel.js
@@ -3,7 +3,7 @@ document.addEventListener('DOMContentLoaded', () => {
     // ===================================================================
     // CONFIGURATION ET ÉLÉMENTS DU DOM
     // ===================================================================
-    const GOOGLE_SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbzt7XFdLdbYePK0WkeNezAlbkZgi2pmCK2G46tgxGCJeP1OGqwpmxC8YYeI5mOh2UcNkA/exec';
+    const GOOGLE_SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbzcnNEXvnH-zbVbBTrVPIrKHztmeyt6qyN0kp_4CKIMas4EJiXh0gGUJONy-EIPQIZXQA/exec';
     const ADMIN_USER = 'admin_gala';
     const ADMIN_PASS = 'P@sswordGala2025!';
 
