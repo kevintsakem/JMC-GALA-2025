4e31bca2b94b460f90aac19e93e30cf4708ee462
Jack kevin Tsakem Kengne <111010770+kevintsakem@users.noreply.github.com>
2025-12-18 21:41:55 +0100

    Change Google Script URL in admin_panel.js
    Updated the Google Apps Script URL for the admin panel.

--- admin_panel.js
+++ admin_panel.js
@@ -1,7 +1,7 @@
 document.addEventListener('DOMContentLoaded', () => {
     // --- IMPORTANT ---
     // Collez ici l'URL de votre application web Google Apps Script
-    const GOOGLE_SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbxLeN19ZEz_UvlOJbIPfArHe_th04cQVu6-EmXX_vCc7ugByKb4aFsaIEh6gx3Mv34ufA/exec';
+    const GOOGLE_SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbw_mRZK-ICcmZ6Vn5xZzcHr7ZMQNyE6qtK93JdbrYMugZuzODVfZftq-Ui1biFPxU4RgA/exec';
 
     // Éléments de la modale de connexion
     const adminLoginBtn = document.getElementById('admin-login-btn');
